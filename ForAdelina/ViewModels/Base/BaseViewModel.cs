c17946e5e3d55553ff02922a5137d7ffcb2bab2c
blrds <blrds@yandex.ru>
2024-03-16 23:40:44 +0700

    Add project files.

---
+++ ForAdelina/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ForAdelina.ViewModels.Base
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, string[] toNotify = null, [CallerMemberName] string propertyNane = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            if(toNotify != null)
+            {
+                foreach(var item in toNotify)
+                {
+                    OnPropertyChanged(item);
+                }
+            }
+            OnPropertyChanged(propertyNane);
+            return true;
+        }
+    }
+}
